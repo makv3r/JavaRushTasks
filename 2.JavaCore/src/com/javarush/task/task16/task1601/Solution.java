056fca96c89102373886629cd2a5be94838ef24a
makv3r <makv3r@gmail.com>
2018-11-10 20:44:40 +0200

    reinitialized files

--- 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
@@ -8,5 +8,6 @@ public class Solution {
     public static void main(String[] args) {
         //TestThread task = new TestThread();
         //new Thread(task).start();
+        //TestCommit
     }
 }
