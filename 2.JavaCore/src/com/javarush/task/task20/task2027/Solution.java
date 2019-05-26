3e69616af9a454461bbf4712df6dcc5f4bdb627b
makv3r <makv3r@gmail.com>
2019-05-26 13:02:19 +0300

    20lvl task2027 Refactored

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -32,168 +32,36 @@ public class Solution {
 
     }
 
-    private static Word checkNE(int[][] crossword, int startX, int startY, String s) {
-        int x = startX - 1;
-        int y = startY - 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (x < 0 || y < 0) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            x--;
-            y--;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x + 1, y + 1);
-        return word;
-    }
-
-    private static Word checkN(int[][] crossword, int startX, int startY, String s) {
-        int x = startX;
-        int y = startY - 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (y < 0) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            y--;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x, y + 1);
-        return word;
-    }
-
-    private static Word checkNW(int[][] crossword, int startX, int startY, String s) {
-        int x = startX + 1;
-        int y = startY - 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (x >= crossword[0].length || y < 0) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            x++;
-            y--;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x - 1, y + 1);
-        return word;
-    }
-
-    private static Word checkE(int[][] crossword, int startX, int startY, String s) {
-        int x = startX + 1;
-        int y = startY;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (x >= crossword[0].length) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            x++;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x - 1, y);
-        return word;
-    }
-
-    private static Word checkSE(int[][] crossword, int startX, int startY, String s) {
-        int x = startX + 1;
-        int y = startY + 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (x >= crossword[0].length || y >= crossword.length) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            x++;
-            y++;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x - 1, y - 1);
-        return word;
-    }
-
-    private static Word checkS(int[][] crossword, int startX, int startY, String s) {
-        int x = startX;
-        int y = startY + 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (y >= crossword.length) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            y++;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x, y - 1);
-        return word;
-    }
-
-    private static Word checkSW(int[][] crossword, int startX, int startY, String s) {
-        int x = startX - 1;
-        int y = startY + 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (x < 0 || y >= crossword.length) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            x--;
-            y++;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x + 1, y - 1);
-        return word;
-    }
-
-    private static Word checkW(int[][] crossword, int startX, int startY, String s) {
-        int x = startX - 1;
-        int y = startY;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (x < 0 || y >= crossword[0].length) return null;
-            if ((char) crossword[y][x] != s.charAt(i)) return null;
-            x--;
-        }
-
-        Word word = new Word(s);
-        word.setStartPoint(startX, startY);
-        word.setEndPoint(x + 1, y);
-        return word;
-    }
-
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
-        ArrayList<Word> result = new ArrayList<>();
+        List<Word> result = new ArrayList<>();
+
+        int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
 
         for (String word : words) {
-            char latter = word.charAt(0);
+            for (int row = 0; row < crossword.length; row++) {
+                for (int col = 0; col < crossword[row].length; col++) {
+                    if ((char) crossword[row][col] == word.charAt(0)) {
 
-            for (int i = 0; i < crossword.length; i++) {
-                for (int j = 0; j < crossword[i].length; j++) {
+                        for (int dir = 0; dir < 8; dir++) {
+                            int k;
+                            int rd = row + x[dir];
+                            int cd = col + y[dir];
 
-                    if ((char) crossword[i][j] == latter) {
-                        int startX = j;
-                        int startY = i;
+                            for (k = 1; k < word.length(); k++) {
+                                if (rd >= crossword.length || rd < 0 || cd >= crossword[0].length || cd < 0) break;
+                                if (crossword[rd][cd] != word.charAt(k)) break;
+                                rd += x[dir];
+                                cd += y[dir];
+                            }
 
-                        Word[] array = new Word[8];
-
-                        array[0] = checkNE(crossword, startX, startY, word);
-                        array[1] = checkN(crossword, startX, startY, word);
-                        array[2] = checkNW(crossword, startX, startY, word);
-                        array[3] = checkE(crossword, startX, startY, word);
-                        array[4] = checkSE(crossword, startX, startY, word);
-                        array[5] = checkS(crossword, startX, startY, word);
-                        array[6] = checkSW(crossword, startX, startY, word);
-                        array[7] = checkW(crossword, startX, startY, word);
-
-                        for (int k = 0; k < array.length; k++) {
-                            if (array[k] != null) {
-                                result.add(array[k]);
+                            if (k == word.length()) {
+                                Word element = new Word(word);
+                                element.setStartPoint(col, row);
+                                element.setEndPoint(cd - y[dir], rd - x[dir]);
+                                result.add(element);
                             }
                         }
-
                     }
                 }
             }
